bcdde8d994885a4686314f1ed8c9b13d2d5559f2
LHNB521 <1767359165@qq.com>
2024-04-11 16:04:04 +0800

    🎇修改配置 tsconfig.json

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+export default[
+  {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+      parser: '@typescript-eslint/parser',
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      jsxPragma: 'React',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    extends: [
+      'plugin:vue/vue3-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:prettier/recommended',
+    ],
+  },
+]
