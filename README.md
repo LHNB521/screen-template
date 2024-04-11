468195441b2d7c8cda1948c3f31dc826fa0ddd36
LHNB521 <1767359165@qq.com>
2024-04-11 15:11:01 +0800

    👓设置 `@` 指向 `src` 目录

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 可视化大屏模板：基于Vu3+typescript+Vite
 
-# 目录
+# 项目目录
+
+
+# 项目模板启动和打包
+1. npm install
+2. npm run dev
+3. npm run build
+
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src') // 设置 `@` 指向 `src` 目录
+    }
+  },
 })
