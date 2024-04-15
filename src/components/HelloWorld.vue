b481ceab1221446d0afb12de401d08c16ac5cd4c
LHNB521 <1767359165@qq.com>
2024-04-15 14:01:52 +0800

    feat: :sparkles: 项目根目录新建 .env.development 、.env.production

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,11 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <div class="card">
-    <el-button type="primary" @click="count++">count is {{ count }}</el-button>
-  </div>
+  <h1>{{ counterStore.count }}</h1>
+  <h1>{{ counterStore.double }}</h1>
 </template>
 
 <script setup lang="ts">
 import { useCounterStore } from '@/store'
 const counterStore = useCounterStore()
-defineProps<{ msg: string }>()
-const count = ref(0)
 </script>
 
 <style scoped lang="scss">
