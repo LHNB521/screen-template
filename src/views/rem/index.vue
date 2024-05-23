82ba6abd77143e8f1c59b7118a0b10427e727b2d
lh <1767359165@qq.com>
2024-05-23 15:30:51 +0800

    feat: 布局修改

--- src/views/rem/index.vue
+++ src/views/rem/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div class="flexible">
     <h1>rem响应式方案</h1>
+    <div class="box1"></div>
+    <div class="box2"></div>
+    <div class="box3"></div>
   </div>
 </template>
 
@@ -9,3 +12,9 @@ import { useFlexible } from '@/hooks/useFlexble'
 // 响应式
 useFlexible(window, document)
 </script>
+<style lang="scss" scoped>
+.flexible {
+  width: 100%;
+  height: 100%;
+}
+</style>
